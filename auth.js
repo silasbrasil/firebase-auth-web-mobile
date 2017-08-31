556d47e0b59a54addf5f705a235f5ec7c1027ace
silasbrasil <silas.swf@gmail.com>
2017-08-31 10:03:29 -0300

    Add email and password authentication, add Google Authetication

--- auth.js
+++ auth.js
@@ -5,3 +5,59 @@
  */
 import firebase from 'firebase'
 import Promise from 'Bluebird'
+
+
+class Auth {
+    /**
+     * Config firebase modules with firebaseConfig and Google PLus Plugin with
+     * webClient
+     * @param {Object} firebaseConfig
+     * @param {String} webClientId
+     */
+    constructor ( firebaseConfig, webClientId ) {
+        this._webClientId = webClientId
+        firebase.initializeApp( firebaseConfig )
+    }
+
+    /**
+     * Get webClientId used on Google Plus Authentication
+     * @return {String} webClientId
+     */
+    get webClientId () { return this._webClientId }
+
+    /**
+     * Loga com Email e Senha
+     *  @param {Object} EmailAndPassword com email e password
+     *  @return {Promise}
+     */
+    signInWithEmailAndPassword ( EmailAndPassword ) {
+        const { email, password } = EmailAndPassword
+        return firebase.auth().signInWithEmailAndPassword( email, password )
+    }
+
+    /**
+     * Login com Google
+     *  @return Promise com User do Firebase
+     */
+    signInWithGoogle () {
+        if ( typeof cordova === 'obejct' ) {
+            return new Promise((resolve, reject) => {
+                window.plugins.googleplus.login(
+                    { 'webClientId': this._webClientId },
+                    result => {
+                        let googleCredential = firebase.auth.GoogleAuthProvider.credential(result.idToken);
+                        firebase.auth().signInWithCredential(googleCredential)
+                            .then(user => resolve(user))
+                            .catch(error => reject(error))
+                    },
+                    error => {
+                        console.log('## ERRO: ', error)
+                        reject(new Error('Não foi possível acessar o Provedor do Google'))
+                    })
+            })
+        } else {
+            let provider = new firebase.auth.GoogleAuthProvider()
+            return firebase.auth().signInWithPopup(provider)
+        }
+    }
+}
